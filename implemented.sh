62f3e5d931de5e42923c1f2e3872653fb6275ec5
Maneren <49210777+Maneren@users.noreply.github.com>
2022-08-03 03:03:33 +0200

    removed old files

--- implemented.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-non_empty=$(cat test/* | grep -ve '^$')
-lines=$(echo "$non_empty" | wc -l)
-uncommented=$(echo "$non_empty" | grep -cv "^\s*//")
-
-echo Implemented: $(( uncommented * 100 / lines )) % of test files
