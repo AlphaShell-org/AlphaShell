9ffb565281a80ad3920cf24ce2653aa14233b943
Maneren <49210777+Maneren@users.noreply.github.com>
2023-03-16 13:52:03 +0100

    allow both ident and string for external

--- src/parse/external.rs
+++ src/parse/external.rs
@@ -10,14 +10,14 @@ pub fn parse(ph: &mut ParseHelper) -> ParserResult<Node> {
   ph.advance();
 
   match ph.peek(0) {
-    Some(TT::String(string)) => ph.variables.insert(string.clone()),
+    Some(TT::Identifier(string) | TT::String(string)) => ph.variables.insert(string.clone()),
     Some(_) => return Err(Error::unexpected(ph)),
     None => return Err(Error::end(ph)),
   };
 
-  ph.advance();
-
   loop {
+    ph.advance();
+
     match ph.peek(0) {
       Some(TT::Comma) => ph.advance(),
       Some(TT::Semicolon) => break,
@@ -25,10 +25,8 @@ pub fn parse(ph: &mut ParseHelper) -> ParserResult<Node> {
       None => return Err(Error::end(ph)),
     };
 
-    ph.advance();
-
     match ph.peek(0) {
-      Some(TT::String(string)) => ph.variables.insert(string.clone()),
+      Some(TT::Identifier(string) | TT::String(string)) => ph.variables.insert(string.clone()),
       Some(_) => return Err(Error::unexpected(ph)),
       None => return Err(Error::end(ph)),
     };
