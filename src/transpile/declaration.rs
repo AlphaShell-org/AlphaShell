8909fca58096c6698be486085f6f330487459b83
Maneren <49210777+Maneren@users.noreply.github.com>
2023-03-26 13:00:53 +0200

    fix maps not getting exported or local

--- src/transpile/declaration.rs
+++ src/transpile/declaration.rs
@@ -16,15 +16,15 @@ pub fn transpile(t: &mut Transpiler, node: &Node) -> TranspileResult<String> {
     value,
   }) = node
   {
-    if let Node::Value(Value::Literal(Literal::Map(_))) = **value {
-      let value = value::transpile(t, value)?;
-      return Ok(format!("typeset -A {name}={value}"));
-    }
-
     let type_string = match r#type {
       Type::Export => "export",
       Type::Let => "local",
     };
+
+    if let Node::Value(Value::Literal(Literal::Map(_))) = **value {
+      let value = value::transpile(t, value)?;
+      return Ok(format!("{type_string} {name}\ntypeset -A {name}={value}"));
+    }
 
     let value = value::transpile(t, value)?;
 
